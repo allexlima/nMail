f768fc7a82bf6dba621a86cbf8f9c81ec6f7c5fc
Allex Lima <allexlima97@gmail.com>
2017-08-26 23:00:38 -0400

    Testing new login method from 'database.User()'

--- db.py
+++ db.py
@@ -4,8 +4,8 @@ from database.base import PostrgeSQL
 import json
 
 if __name__ == "__main__":
-    a = User()
-    a.db_connect()
-    print(a.show())
-
-
+    user = User()
+    user.db_connect()
+    # user.change(34, new_name='Allex Lima', is_admin=True, is_activated=True)
+    print(user.list(34))
+    print(user.login('allexlima97@gmail.com', '123'))
