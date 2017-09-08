0ab59fe72e2eb44c6ef047b6b854ea3e1e4c77e1
Allex Lima <allexlima97@gmail.com>
2017-09-08 18:50:03 -0400

    fixing 'db_connect' and 'query' methods

--- nmail/database/base.py
+++ nmail/database/base.py
@@ -7,31 +7,29 @@ class PostrgeSQL:
     def __init__(self):
         self.__conn = None
         self.__log = []
+        self.__config = None
 
-    @staticmethod
-    def __config():
+    def db_config(self, file="settings.cfg"):
         aux = configparser.ConfigParser()
-        aux.read("database/settings.cfg")
-        return tuple(aux['Database'][i] for i in aux['Database'])
+        aux.read(file)
+        self.__config = tuple(aux['Database'][i] for i in aux['Database'])
 
     def db_connect(self):
-        try:
-            self.__conn = psql.connect("user={} password={} dbname={} host={} port={}".format(*self.__config()))
-        except Exception as error:
-            print(error)
-        return self if self.__conn else None
+        self.__conn = psql.connect("user={} password={} dbname={} host={} port={}".format(*self.__config))
+        return self
 
     def db_disconnect(self):
-        return self.__conn.close() if self.__conn else False
+        return self.__conn.close() if self.__conn.status else False
 
     def query(self, sql, params=None, commit=False, fetch=False):
         feedback = None
         try:
             cursor = self.__conn.cursor(cursor_factory=DictCursor)
             cursor.execute(sql, params)
+            feedback = cursor.fetchall() if fetch is True else None
             if commit is True:
                 self.__conn.commit()
-            feedback = cursor.fetchall() if fetch is True else None
+                feedback = True if feedback is None else feedback
             cursor.close()
         except Exception:
             if commit is True:
