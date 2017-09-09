dc7b3370556e6edc5deb721de2f725fab40e23d6
Allex Lima <allexlima97@gmail.com>
2017-09-08 20:47:57 -0400

    change project structure

---
+++ nmail/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restful import Api
+
+from nmail.resources.common import Home
+from nmail.resources.user import UserAPI
+
+app = Flask("nmail")
+api = Api(app, catch_all_404s=True)
+
+api.add_resource(Home, '/', '/index/', '/default/', '/home/', '/api/')
+api.add_resource(UserAPI, '/api/user/', '/api/user/<int:user_id>/')
